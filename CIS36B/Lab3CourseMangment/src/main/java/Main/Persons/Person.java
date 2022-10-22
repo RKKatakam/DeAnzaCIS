d83412b87dd9546ca8f459351bc95477767f06b7
kittukatakam <katakamraghavakrishna@student.deanza.edu>
2022-10-21 17:46:45 -0700

    created person class

---
+++ CIS36B/Lab3CourseMangment/src/main/java/Main/Persons/Person.java
@@ -0,0 +1,22 @@
+package Main.Persons;
+
+import java.util.UUID;
+
+public class Person {
+    private final String name;
+    private final String id;
+
+    public Person(String name) {
+        this.name = name;
+        this.id = UUID.randomUUID().toString();
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public String getId() {
+        return id;
+    }
+}
---
+++ CIS36B/Lab3CourseMangment/src/main/java/Main/Persons/Professor.java
@@ -0,0 +1,9 @@
+package Main.Persons;
+
+public class Professor extends Person {
+
+    //todo: add all the specifics of professor
+    public Professor(String name) {
+        super(name);
+    }
+}
